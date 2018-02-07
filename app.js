06701493da9bf741f6eb7143f05014614d0a859a
nicolegager <ngager23@gmail.com>
2018-02-07 13:04:58 -0500

    Code to connect to DB via Mongoose

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const expresshbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 5000;
 
+// connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
-app.engine('handlebars', expresshbs({defaultLayout: 'main'}));
+// handlebars middleware
+app.engine('handlebars', expresshbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // define the index route
 app.get('/', (request, response) => {
   const title = 'Welcome';
-  response.render('index', {title: title});
+  response.render('index', { title: title });
 });
 
 app.get('/about', (request, response) => {
@@ -19,5 +25,5 @@ app.get('/about', (request, response) => {
 });
 
 app.listen(port, () => {
-   console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
