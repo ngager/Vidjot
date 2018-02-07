c4b0b1e1f4c1046b9ce9b96641907634e79a95b0
nicolegager <ngager23@gmail.com>
2018-02-07 14:54:10 -0500

    Add idea form

--- app.js
+++ app.js
@@ -10,18 +10,28 @@ mongoose.connect('mongodb://localhost/vidjot-dev')
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
+// load idea model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 // handlebars middleware
 app.engine('handlebars', expresshbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// define the index route
+// define index route
 app.get('/', (request, response) => {
   const title = 'Welcome';
   response.render('index', { title: title });
 });
 
+// define about route
 app.get('/about', (request, response) => {
   response.render('about');
+});
+
+// define idea form
+app.get('/ideas/add', (request, response) => {
+  response.render('ideas/add');
 });
 
 app.listen(port, () => {
