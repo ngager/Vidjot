ea5f2b534b6b39c108fea693e1cc9af142c7a6b8
nicolegager <ngager23@gmail.com>
2018-02-11 13:41:46 -0500

    Adding flash and session middleware

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const expresshbs = require('express-handlebars');
-const bodyparser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+
 
 const app = express();
 const port = 5000;
@@ -21,11 +24,28 @@ app.engine('handlebars', expresshbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // body parser middleware
-app.use(bodyparser.urlencoded({ extended: false }));
-app.use(bodyparser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // method override middleware
 app.use(methodOverride('_method'));
+
+// express session middleware
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+
+// global variables
+app.use((request, response, next) => {
+  response.locals.success_msg = request.flash('success_msg');
+  response.locals.error_msg = request.flash('error_msg');
+  response.locals.error = request.flash('error');
+  next();
+});
 
 // define about route
 app.get('/about', (request, response) => {
