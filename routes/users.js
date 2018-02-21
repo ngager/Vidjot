6a8c4602b1548835a255b9cea3906129b39a1890
nicolegager <ngager23@gmail.com>
2018-02-20 22:54:32 -0500

    Add client side validation for register form

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,33 @@ router.get('/login', (request, response) => {
   response.render('users/login');
 });
 
-// user registration
+// user registration route
 router.get('/register', (request, response) => {
   response.render('users/register');
+});
+
+// user registration form POST
+router.post('/register', (request, response) => {
+  let errors = [];
+
+  if (request.body.password != request.body.passwordConfirmation) {
+    errors.push({ text: 'Passwords do not match.'});
+  }
+
+  if (request.body.password.length < 4) {
+    errors.push({ text: 'Password must be at least 4 characters.' });
+  }
+
+  if (errors.length > 0) {
+    response.render('users/register', {
+      errors: errors,
+      name: request.body.name,
+      email: request.body.email,
+      password: request.body.password,
+      passwordConfirmation: request.body.passwordConfirmation
+    });
+  } else {
+    response.send('PASS');
+  }
+
 });
